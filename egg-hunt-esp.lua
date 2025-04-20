f0b446e3b05f8ebf24c1870a5f50efa9afaf8f20
ryleyw8412 <71460861+ryleyw8412@users.noreply.github.com>
2025-04-20 15:15:25 +0100

    Update egg-hunt-esp.lua
    it works now hah

--- egg-hunt-esp.lua
+++ egg-hunt-esp.lua
@@ -1,8 +1,17 @@
-for i, v in game.Workspace:WaitForChild("EggHunt"):GetDescendants() do
+while true do
+    for i, v in game.Workspace:WaitForChild("EggHunt"):GetChildren() do
+        for ii, vv in v:GetChildren() do
+            if v:IsA("Highlight") then
+            v:Destroy()
+            end
+        end
+
     local high = Instance.new("Highlight")
     high.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
     high.OutlineColor = Color3.fromRGB(255, 255, 255)
     high.FillColor = Color3.fromRGB(255, 0, 0)
 
     high.Parent = v
+    end
+    task.wait(0.1)
 end
