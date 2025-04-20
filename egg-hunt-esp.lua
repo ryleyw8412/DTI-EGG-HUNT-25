b6f18f3fbf29fb52c87d559179eefd14679afa12
ryleyw8412 <71460861+ryleyw8412@users.noreply.github.com>
2025-04-20 11:16:55 +0100

    Create egg-hunt-esp.lua

---
+++ egg-hunt-esp.lua
@@ -0,0 +1,8 @@
+for i, v in game.Workspace:WaitForChild("EggHunt"):GetChildren() do
+    local high = Instance.new("Highlight")
+    high.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    high.OutlineColor = Color3.fromRGB(255, 255, 255)
+    high.FillColor = Color3.fromRGB(255, 0, 0)
+
+    high.Parent = v
+end
